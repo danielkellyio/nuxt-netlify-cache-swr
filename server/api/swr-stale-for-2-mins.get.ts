6fd843ad82b4f46205f871509a0beda026a529fd
Daniel Kelly <me@danielkelly.io>
2024-04-19 08:58:12 -0500

    stale for 2

--- server/api/swr-stale-for-2-mins.get.ts
+++ server/api/swr-stale-for-2-mins.get.ts
@@ -1,10 +1,13 @@
 import { setHeaders, getQuery } from "h3";
 export default defineEventHandler(async (event) => {
   const query = getQuery(event);
+  const minutes = 2;
+
   setHeaders(event, {
     "Cache-Control": "public, max-age=0, must-revalidate", // Tell browsers to always revalidate
-    "Netlify-CDN-Cache-Control":
-      "public, max-age=0, stale-while-revalidate=31536000", // Tell Edge to cache asset for up to a year
+    "Netlify-CDN-Cache-Control": `public, max-age=${
+      minutes * 60
+    }, stale-while-revalidate=31536000`, // Tell Edge to cache asset for up to a year
   });
   return {
     time: new Date(),
